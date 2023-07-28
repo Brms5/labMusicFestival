6c94d654508e85dc6d2836ac7c8a78ce1a015b8c
Bruno <moreirasbr@gmail.com>
2023-07-28 18:39:50 -0300

    feat: background image

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import { GlobalStyle } from "@ui/style/GlobalStyle";
 import { Header, Main, Title } from "./style";
 
-const bg = "/rockbands.png";
+const bg = "/music-png.png";
 
 function Page() {
   console.log("bg", bg);
   return (
-    <Main>
+    <Main
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <GlobalStyle />
       <Header>
         <Title>LIVE MUSIC</Title>
--- pages/style.ts
+++ pages/style.ts
@@ -8,7 +8,6 @@ export const Main = styled.main`
 
 export const Header = styled.header`
   display: flex;
-  /* flex-direction: column; */
   align-items: center;
   justify-content: center;
   width: 100%;
